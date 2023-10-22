b910d7ac2b23e48cc696a33a7028dbd45291aa0f
Toxicwhisper <morgan.lindzen@gmail.com>
2023-10-22 21:07:19 +0200

    basic configuration, reviewing endpoints, entrances, etc. - problem with visability and no errors fixed

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { moviesAPI } from './api'; 
+import { moviesAPI } from './api';  
 import axios from 'axios';
 function MovieList() {
   const [movies, setMovies] = useState([]);
